e4ef12173584c6d0d0b093e142cc56226d1e4da8
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:22:36 +0800

    chore: disable index for CRA-FTP compatible

---
+++ src/pages/sitehome/index.js
@@ -0,0 +1,12 @@
+// export * from '/sitehome/static-nav-msg/staticnavmsg';
+// export * from '/sitehome/uppernav/uppernav';
+// export * from '/sitehome/main';
+// export * from '/sitehome/sitehero/sitehero';
+// export * from '/sitehome/brandarea/brandarea';
+// export * from '/sitehome/featuredspecs/featuredSpecs';
+// export * from '/sitehome/productcarousel/productcarousel';
+// export * from '/sitehome/serviceofferings/serviceofferings';
+// export * from '/sitehome/productspecs/productspecs';
+// export * from '/sitehome/websiteportfolio/websiteportfolio';
+// export * from '/sitehome/calltoaction/calltoaction';
+
