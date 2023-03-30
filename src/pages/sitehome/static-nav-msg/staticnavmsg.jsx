d783acafa4a194709ada91e599a566fbf81ad1c6
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:50:32 +0800

    chore: staticnavmsg jsx migration to CRA-FTP compatible

---
+++ src/pages/sitehome/static-nav-msg/staticnavmsg.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { staticNavMsg } from '../../../data/sitehome';
+import { staticNavMsg } from 'data/sitehome';
+// import { staticNavMsg } from '../../../data/sitehome';
+import './staticnavmsg.scss';
+
+
+export function StaticNavMsg() {
+    return (
+        <React.Fragment>
+        <div className='staticnavmsg-wrap'>
+            <p className='staticnavmsg-msg'>
+                {staticNavMsg.msg1}
+            </p>
+        </div>
+        </React.Fragment>
+    );
+}
+
+
+export default StaticNavMsg;
