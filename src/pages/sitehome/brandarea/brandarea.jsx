37a5f21ba8677f3bf7e35659a73ed857dd042973
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:48:35 +0800

    chore: brandarea jsx migration to CRA-FTP compatible

---
+++ src/pages/sitehome/brandarea/brandarea.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import SectionTitle from '../components/sectionTitle/sectionTitle';
+import SectionTitle from '../../../components/sectionTitle/sectionTitle';
+import { responsiveBrands, sectionTitles } from '../../../data/sitehome';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import './brandarea.scss';
+
+
+
+export function BrandArea(props) {
+    const brandArray = Object.values(props.brands);
+    const { featMsg, title, desc } = sectionTitles[0];
+    const handleDragStart = (e) => e.preventDefault();
+    const items2 = brandArray.map(({id, src}) => (
+      <img className={'brand-area-item'} src={src} key={id} onDragStart={handleDragStart} role='presentation'/>
+    ));
+  return (
+    <React.Fragment>
+        <SectionTitle featMsg={featMsg} title={title} desc={desc} marginBottom={'40'}/>
+        <div className='carousel-wrap'>
+            <AliceCarousel 
+            mouseTracking
+            autoPlay
+            infinite
+            autoPlayInterval = {4500}
+            animationDuration = {1000}
+            disableDotsControls
+            disableButtonsControls 
+            items={items2} 
+            responsive={responsiveBrands}
+            />
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default BrandArea;
