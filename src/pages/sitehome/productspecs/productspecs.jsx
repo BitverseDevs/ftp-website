05dc8450ce375cc9dbc830ab967eadab2adbcb7a
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:49:28 +0800

    chore: productspecs jsx migration to CRA-FTP compatible

---
+++ src/pages/sitehome/productspecs/productspecs.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './productspecs.scss';
+// import SectionTitle from '../components/sectionTitle/sectionTitle';
+import FeaturesList from 'components/featureslist/featureslist';
+import SectionTitle from 'components/sectionTitle/sectionTitle';
+
+
+export function ProductSpecs(props){ 
+    const {featMsg, title, desc} = props?.productsTitle;
+
+
+    return(
+        <React.Fragment>
+            <section className='product-specs--section'>
+                <h2 className='product-specs--h2'>
+                    <SectionTitle featMsg={featMsg} title={title} desc={desc}/>
+                </h2>
+                <summary className='product-specs--summary'>
+                    <FeaturesList products={props?.productsList}/>
+                </summary>
+            </section>
+        </React.Fragment>
+    )
+}
+
+
+export default ProductSpecs;
