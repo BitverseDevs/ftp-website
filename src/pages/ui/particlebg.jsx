244a1316de44c6427189449e679da1dce0398cbb
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:51:16 +0800

    chore: particlebg jsx migration to CRA-FTP compatible

---
+++ src/pages/ui/particlebg.jsx
@@ -0,0 +1,104 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import './particlebg.scss';
+
+export const ParticleBg = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#eff9f3",
+                        // value: "#000",
+                    },
+                },
+                fpsLimit: 1200,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: false,
+                            // mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#0063FF",
+                        // value: "#FFFFFF",
+                    },
+                    links: {
+                        color: "#D3D3D3",
+                        // color: "#FFFFFF",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: {min: 1, max: 2},
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 1000,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.3,
+                    },
+                    shape: {
+                        type: "edge",
+                    },
+                    size: {
+                        value: { min: 1, max: 6 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
+
+
+export default ParticleBg;
