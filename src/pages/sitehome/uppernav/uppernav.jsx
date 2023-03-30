bce588234a719b5dc0cfe9f25d7d9ed85b97c6a3
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:50:43 +0800

    chore: uppernav jsx migration to CRA-FTP compatible

---
+++ src/pages/sitehome/uppernav/uppernav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { upperNavData } from '../../../data/sitehome';
+import { upperNavData } from 'data/sitehome';
+import './uppernav.scss';
+
+
+export function Uppernav() {
+    return (
+        <React.Fragment>
+        <div className='uppernav-wrap'>
+            <div className='uppernav-logo'>
+                <img src='/assets/bitverse-logo-darkblue-blue.png'/>
+            </div>
+            <div className='uppernav-navItems'>
+                <p>{upperNavData.nav1}</p>
+                <p>{upperNavData.nav2}</p>
+                <p>{upperNavData.nav3}</p>
+                <p>{upperNavData.nav4}</p>
+            </div>
+            <button className='uppernav-button'>
+                {upperNavData.button}
+            </button>
+        </div>
+        </React.Fragment>
+    );
+}
+
+
+export default Uppernav;
