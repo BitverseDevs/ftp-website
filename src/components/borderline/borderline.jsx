f1888854538f42ee2815cc05b936dd4e3c489e16
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-30 17:17:10 +0800

    chore: migration borderline component

---
+++ src/components/borderline/borderline.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { borderLine } from '../../../../data/uiData';
+// import borderLine from '../../data/uiData';
+import { borderLine } from 'data/uiData';
+import './borderline.scss';
+
+
+
+export function BorderLine(props) {
+    const custWidth = props?.custWidth;
+    const firstShape = borderLine[0];
+    const secondShape = borderLine[1];
+    return (
+        <React.Fragment>
+            <section className='borderline-container' style={{width: `${custWidth}px`}}>
+                <div className='borderline-line-bg'></div>
+                <div className='borderline-content'>
+                    <img className='borderline-sh1' src={firstShape?.assetSrc} alt={firstShape?.alt} key={firstShape?.id+'1'}/>
+                    <img className='borderline-sh2' src={secondShape?.assetSrc} alt={secondShape?.alt} key={secondShape?.id+'2'}/>
+                    <img className='borderline-sh1' src={firstShape?.assetSrc} alt={firstShape?.alt} key={firstShape?.id+'3'}/>
+                </div>
+            </section>
+        </React.Fragment>
+    );
+}
+
+export default BorderLine;
