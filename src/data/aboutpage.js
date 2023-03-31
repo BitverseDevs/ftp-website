fc585d8baabc91cddb9a56319908d34f6624713a
Mattttyyyy <wehttamocsalb@gmail.com>
2023-03-31 18:30:20 +0800

    feat: about page assets

---
+++ src/data/aboutpage.js
@@ -0,0 +1,75 @@
+export const aboutPageData = [
+    {
+        key: 'apd01',
+        title: 'Bitverse Corporation',
+        isMainAbout: true,
+        picUrl: '/assets/z-bv-about.png',
+        desc: 'We are an information system solutions provider with core expertise on corporate automation platforms and software development. Our collective experience creating and delivering robust IT solutions for local and global companies across various industries: Telecommunications I Government I Healthcare I Real Estate I Construction ITransportation I Food Retail I Education I Logistics',
+    },
+    {
+        key: 'apd02',
+        title: 'Our Mission',
+        isMainAbout: false,
+        picUrl: '',
+        desc: 'To ensure the success of our partners by providing continuing and professional IT support, using our expertiseto long-term, innovative and cost-effective solutions for our partners.',
+    },
+    {
+        key: 'apd03',
+        title: 'Our Vision',
+        isMainAbout: false,
+        picUrl: '',
+        desc: 'To be trusted partner of choice providing top-notch, innovative and reliable IT solutions for our customers both local and international.',
+    },
+    {
+        key: 'apd04',
+        title: 'Our Focus',
+        isMainAbout: false,
+        picUrl: '',
+        desc: 'Forge strategic partnerships with progressive and pioneering organizations that are in the forefront of utilizing technology to deliver differentiation to their business. Transform organizations via innovative technological solutions. Enable partners to move to automated and digital systems.',
+    }
+];
+
+export const aboutPillarsTitle = {
+    featMsg: 'WHY BITVERSE',
+    title: '4 Key Pillars',
+    desc: 'Consistent in every client engagement.'
+};
+
+export const aboutPillarsList = [
+{
+    id: 'apl01',
+    productName: 'Expertise',
+    productDesc: 'Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola.',
+    srcLink: '#',
+},
+{
+    id: 'apl02',
+    productName: 'Quality and Consistency',
+    productDesc: 'Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola.',
+    srcLink: '#',
+},
+{
+    id: 'apl03',
+    productName: 'Cost Efficiency',
+    productDesc: 'Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola.',
+    srcLink: '#',
+},
+{
+    id: 'apl04',
+    productName: 'Transparency',
+    productDesc: 'Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola.',
+    srcLink: '#',
+},
+];
+
+export const aboutPageBanner = {
+    bannerSrc: '/assets/z-bv-banner-about.png'
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    aboutPageData,
+    aboutPageBanner,
+    aboutPillarsTitle,
+    aboutPillarsList,
+}
