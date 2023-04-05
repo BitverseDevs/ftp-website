f70c8b2cec557c02d398d6023ede200c290c8376
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-05 10:38:32 +0800

    feat: contact us page init commit

--- src/pages/contactus/contactus.jsx
+++ src/pages/contactus/contactus.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './contactus.scss'
 import ParticleBg from 'pages/ui/particlebg';
 import { aboutPageData, aboutPageBanner, aboutPillarsTitle, aboutPillarsList } from 'data/contactpage';
@@ -6,6 +7,40 @@ import ProductSpecs from 'pages/sitehome/productspecs/productspecs';
 
 export function ContactUs() {
     // console.log(window, 'meow')
+    const [formValues, setFormValues] = useState({
+        fullName: '',
+        email: '',
+        contactNumber: '',
+        companyName: '',
+        numEmployees: ''
+    });
+
+    // Handle input field changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+    console.log(formValues, 'meow?')
+    // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('https://bitverse-api.herokuapp.com/send-email', formValues);
+            console.log(response.data);
+            alert('Email sent successfully');
+            setFormValues({
+                fullName: '',
+                email: '',
+                contactNumber: '',
+                companyName: '',
+                numEmployees: ''
+            });
+        } catch (error) {
+            console.error(error);
+            alert('Error sending email');
+        }
+    };
     return (
         <React.Fragment>
             <ParticleBg/>
@@ -14,10 +49,9 @@ export function ContactUs() {
             </section>
             <section className='contact-us--section-about'>
                 <div className='contact-us--left-section'>
-                    <div className='contact-us--about-logo'>
-                        {/* {'b'}<p>it</p>{'verse'}<p className='contact-us-logo--symbol'>{'>>'}</p> */}
-                        {'Contact Us'}
-                    </div>
+                    {/* <div className='contact-us--about-logo'>
+                        {'b'}<p>it</p>{'verse'}<p className='contact-us-logo--symbol'>{'>>'}</p>
+                    </div> */}
                     <div className='contact-us--main-header'>
                         {aboutPageData.map(({key, title, isMainAbout, picUrl, desc})=>{
                             if(isMainAbout === false){
@@ -44,7 +78,7 @@ export function ContactUs() {
                     </div>
                 </div>
                 <div className='contact-us--right-section'>
-                    <div className='contact-us--right-content'>
+                    {/* <div className='contact-us--right-content'>
                         {aboutPageData.map(({key, title, isMainAbout, picUrl, desc})=>{
                             if(isMainAbout === true){
                                 return;
@@ -65,10 +99,63 @@ export function ContactUs() {
                             }
 
                         })}
-                    </div>
+                    </div> */}
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                        <label>Full Name:</label>
+                        <input
+                            type="text"
+                            name="fullName"
+                            value={formValues.fullName}
+                            onChange={handleChange}
+                            required
+                        />
+                        </div>
+                        <div>
+                        <label>Email Address:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={formValues.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        </div>
+                        <div>
+                        <label>Contact Number:</label>
+                        <input
+                            type="text"
+                            name="contactNumber"
+                            value={formValues.contactNumber}
+                            onChange={handleChange}
+                            required
+                        />
+                        </div>
+                        <div>
+                        <label>Company Name:</label>
+                        <input
+                            type="text"
+                            name="companyName"
+                            value={formValues.companyName}
+                            onChange={handleChange}
+                            required
+                        />
+                        </div>
+                        <div>
+                        <label>Number of Employees:</label>
+                        <input
+                            type="number"
+                            name="numEmployees"
+                            value={formValues.numEmployees}
+                            onChange={handleChange}
+                            required
+                        />
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
             </section>
-            <ProductSpecs productsTitle={aboutPillarsTitle} productsList={aboutPillarsList}/>
+            {/* <ProductSpecs productsTitle={aboutPillarsTitle} productsList={aboutPillarsList}/> */}
         </React.Fragment>
         
     );
