c7bf27b6feffb458fa81104e399ca82706baa9d7
Mattttyyyy <wehttamocsalb@gmail.com>
2023-04-11 15:22:52 +0800

    feat: contactinput complete

---
+++ src/pages/contactus/contactinput/contactinput.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './contactinput.scss';
+
+export function ContactInput(props) {
+    const { 
+        label,
+        labelFor,
+        type,
+        onChange,
+        placeholder,
+        value,
+        required,
+        pattern
+    } = props;
+    // console.log(props, "aaaaa")
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          event.preventDefault();
+        }
+    };
+
+    return (
+        <React.Fragment>
+            <label htmlFor={labelFor} className='contact-input--label'>{label}</label>
+            <input
+            className='contact-input--input'
+            id={labelFor}
+            name={labelFor}
+            onChange={onChange}
+            placeholder={placeholder}
+            type={type}
+            value={value}
+            {...required? required: ''}
+            {...pattern? pattern: ''}
+            onKeyDown={handleKeyDown}
+            />
+        </React.Fragment>
+    );
+}
+
+export default ContactInput;
