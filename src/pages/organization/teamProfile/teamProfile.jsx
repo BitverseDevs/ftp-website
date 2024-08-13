2c83e24bd73ace68018e3e79cb0c725732649bb1
jhumer-apus <jhumerapus@gmail.com>
2024-08-13 14:54:28 +0800

    Updated

--- src/pages/organization/teamProfile/teamProfile.jsx
+++ src/pages/organization/teamProfile/teamProfile.jsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import react, { Fragment } from 'react';
 import './teamProfile.scss';
 import {teamData, internData} from '../../../data/team';
 import ParticleBg from 'pages/ui/particlebg';
@@ -11,10 +11,10 @@ const TeamProfile = (props) => {
     const teamMember = teamData.find(member => member.key === id) ? teamData.find(member => member.key === id) : internData.find(member => member.key === id);
 
     return (    
-        <>
+        <Fragment>
             <div className="team-profile--wrap">
-                <div className="team-profile--content">
-                    <div className="team-profile--img">
+                <div className="flex md:flex-row flex-col gap-8 p-12 items-center max-w-[1400px]">
+                    <div className="rounded-full overflow-hidden w-80 h-80">
                         <img src={teamMember.imgURL} alt="profile"/>
                     </div>
 
@@ -36,7 +36,7 @@ const TeamProfile = (props) => {
                 </div>
             </div>
             <ParticleBg/>
-        </>
+        </Fragment>
     );
 };
 
